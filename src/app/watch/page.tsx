12bf1d801f86adba6d5cf5790d75a463dce83f80
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2023-08-02 22:39:28 +0300

    Update shipping service logo

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import { Button, Row, Space } from "antd";
 
 import Chessboard from "@/components/Chessboard";
@@ -20,21 +21,53 @@ const WatchPage = () => {
           <Chessboard />
           <Advantages />
           <PriceBlock />
-          {/* <div
-            style={{
-              margin: "0 auto",
-              width: "800px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          > */}{" "}
+
           <Row justify="center">
             <Space.Compact>
-              <Button type="primary" size="large">
-                Новапошта
+              <Button
+                type="default"
+                size="large"
+                style={{
+                  color: "#CA3B2B",
+                  fontStyle: "bold",
+                  lineHeight: "24px",
+                  fontSize: "20px",
+                  borderColor: "#CA3B2B",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+                icon={
+                  <Image
+                    src="/assets/img/svg/common/NovaPoshtaLogo.svg"
+                    alt={"Нова пошта логотип"}
+                    width={24}
+                    height={24}
+                  />
+                }
+              >
+                Нова пошта
               </Button>
-              <Button type="primary" size="large">
+              <Button
+                type="default"
+                size="large"
+                style={{
+                  // color: "#F5C258",
+                  fontStyle: "bold",
+                  fontSize: "20px",
+                  lineHeight: "24px",
+                  // borderColor: "#F5C258",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+                icon={
+                  <Image
+                    src="/assets/img/svg/common/UkrPoshtaLogo.svg"
+                    alt={"Укрпошта логотип"}
+                    width={24}
+                    height={24}
+                  />
+                }
+              >
                 Укрпошта
               </Button>
             </Space.Compact>
