78b4bc8f71c8c508e8845d6fb7b4c17ef2190031
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2023-08-02 23:04:28 +0300

    Test disable scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
       </Head>
       <body className={scada.className} style={{ padding: 0, margin: 0 }}>
