2ca5fee8610c0e1cec3122f6489f03e673c2fc82
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2023-08-05 15:17:47 +0300

    Update orderModal for send message to bot

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -1,14 +1,18 @@
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Divider,
+  Input,
+  List,
   Modal,
+  Result,
   Row,
+  Select,
   Space,
   Steps,
   Tabs,
   TabsProps,
 } from "antd";
-import React, { useState } from "react";
 import Image from "next/image";
 import LocationInfo from "../LocationInfo";
 import NovaPoshtaAreasContainer from "@/containers/NovaPoshtaAreasContainer";
@@ -20,29 +24,41 @@ interface OrderModalProps {
   setIsModalOpen: (isOpen: boolean) => void;
 }
 
-const description = "This is a description.";
-
 const OrderModal: React.FC<OrderModalProps> = ({
   isModalOpen,
   setIsModalOpen,
 }) => {
-  const [currentTab, setCurrentTab] = useState<"nova" | "ukr">("nova");
-  const [ukr, setUkr] = useState();
+  const [selectedShippingTab, setSelectedShippingTab] = useState<
+    "nova" | "ukr"
+  >("nova");
+  const [ukr, setUkr] = useState<string | undefined>();
+  const [nova, setNova] = useState<string | undefined>();
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [currentStep, setCurrentStep] = useState(0);
+  const [okText, setOkText] = useState("Далі");
 
   const handleTabChange = (tab: "nova" | "ukr") => {
-    setCurrentTab(tab);
+    setSelectedShippingTab(tab);
     // Perform any other logic here
   };
+
   const items: TabsProps["items"] = [
     {
       key: "nova",
       label: `Tab 1`,
       children: (
-        <NovaPoshtaAreasContainer
-          onSettlementSelected={function (settlementRef: string): void {
-            throw new Error("Function onSettlementSelected not implemented.");
-          }}
+        <TextArea
+          value={nova}
+          onChange={(e) => setNova(e.target.value)}
+          placeholder="Адреса на доставку Нова Пошта"
+          autoSize={{ minRows: 3, maxRows: 5 }}
         />
+        // <NovaPoshtaAreasContainer
+        //   onSettlementSelected={(settlementRef: string): void => {
+        //     throw new Error("Function onSettlementSelected not implemented.");
+        //   }}
+        // />
       ),
     },
     {
@@ -52,65 +68,217 @@ const OrderModal: React.FC<OrderModalProps> = ({
         <TextArea
           value={ukr}
           onChange={(e) => setUkr(e.target.value)}
-          placeholder="Адреса на доставку Укрпоштою"
+          placeholder="Адреса на доставку Укрпошта"
           autoSize={{ minRows: 3, maxRows: 5 }}
         />
       ),
     },
   ];
 
-  const handleOk = () => {
+  const sendOrderData = async () => {
+    console.log(selectedShippingTab, selectedShippingTab == "nova");
+    const orderDetails = {
+      product: `Продукт: Годинник Cheetah Black/Безкоштовна доставка/Подарункова упаковка`,
+      name: `Замовник: ${name}`,
+      phone: `Номер телефону: ${phoneNumber}`,
+      shipping: `Доставка: ${
+        selectedShippingTab === "nova"
+          ? `Нова Пошта - ${nova}`
+          : `Укрпошта - ${ukr}`
+      }`,
+      price: `1799грн`,
+    };
+
+    const webhookUrl = `${process.env.NEXT_PUBLIC_TG_BOT_URL}/order`; // Replace with your webhook URL
+
+    try {
+      const response = await fetch(webhookUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderDetails),
+      });
+
+      const data = await response.json();
+      console.log("Order details sent to Telegram bot:", data);
+    } catch (error) {
+      console.error("Error sending order details:", error);
+    }
+  };
+
+  const handleOk = async () => {
     // Handle OK action here
-    setIsModalOpen(false);
+
+    if (currentStep < 3) {
+      if (currentStep === 1) setOkText("Замовити");
+      if (currentStep === 2) {
+        try {
+          sendOrderData();
+          setOkText("Закрити");
+        } catch {
+          console.error("Order not sent");
+        }
+      }
+      setCurrentStep(currentStep + 1);
+    } else {
+      setIsModalOpen(false);
+      setCurrentStep(0);
+      setOkText("Далі");
+    }
   };
 
   const handleCancel = () => {
-    // Handle Cancel action here
-    setIsModalOpen(false);
+    if (currentStep < 3 && currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    } else {
+      setIsModalOpen(false);
+      setCurrentStep(0);
+    }
+  };
+
+  const renderContent = () => {
+    switch (currentStep) {
+      case 0:
+        return (
+          <>
+            <ShippingTabButtons
+              currentTab={selectedShippingTab}
+              handleTabChange={handleTabChange}
+            />
+            <Tabs
+              activeKey={selectedShippingTab}
+              items={items}
+              style={{ margin: "1.5rem 0" }}
+              renderTabBar={() => <></>}
+              animated
+            />
+            {/* <LocationInfo /> */}
+          </>
+        );
+      case 1:
+        return (
+          <div>
+            <label>Ім'я/Прізвище</label>
+            <Input
+              style={{ marginBottom: "1rem" }}
+              placeholder="Введіть ваше ім'я"
+              pattern="[A-Za-z]*"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Номер телефону</label>
+            <Input
+              addonBefore="+380"
+              maxLength={9}
+              placeholder="Введіть номер"
+              pattern="[0-9]*"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+        );
+      case 2:
+        return (
+          <Space
+            direction="vertical"
+            size="small"
+            style={{ marginBottom: "1.5rem" }}
+          >
+            <Divider orientation="left">Ваше замовлення</Divider>
+            <List
+              size="large"
+              // header={<div>Header</div>}
+              // footer={<div>Footer</div>}
+              bordered
+              dataSource={[
+                `Продукт: Годинник Cheetah Black/Безкоштовна доставка/Подарункова упаковка`,
+                `Замовник: ${name}`,
+                `Номер телефону: ${phoneNumber}`,
+                `Доставка: ${
+                  selectedShippingTab === "nova"
+                    ? `Нова Пошта - ${nova}`
+                    : `Укрпошта - ${ukr}`
+                }`,
+                `Ціна до сплати на відділенні(+комісія пошти): 1799грн`,
+              ]}
+              renderItem={(item: string) => <List.Item>{item}</List.Item>}
+            />
+            {/* <Text>{`Продукт: Годинник Cheetah Black/Безкоштовна доставка/Подарункова упаковка`}</Text>
+            <Text>{`Замовник: ${name}`}</Text>
+            <Text>{`Номер телефону: ${phoneNumber}`}</Text>
+            <Text>{`Доставка: ${
+              selectedShippingTab === "nova"
+                ? `Нова Пошта - ${nova}`
+                : `Укрпошта - ${ukr}`
+            }`}</Text>
+            <Text>{`Ціна до сплати на відділенні(+комісія пошти): 1799грн`}</Text> */}
+          </Space>
+        );
+      case 3:
+        return (
+          <Result
+            status="success"
+            title="Дякуємо за ваш вибір!"
+            subTitle="Ми відправимо ваш годинний найближчим часом, очікуйте повідомлення з накладною."
+            // extra={[
+            //   <Button type="primary" key="console">
+            //     Go Console
+            //   </Button>,
+            //   <Button key="buy">Buy Again</Button>,
+            // ]}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  const renderStepsAndDivider = () => {
+    if (currentStep < 3) {
+      return (
+        <>
+          <Steps
+            current={currentStep}
+            items={[
+              {
+                title: "Доставка",
+                description: "Оберіть спосіб доставки",
+              },
+              {
+                title: "Контакти",
+                description: "Заповніть контактні данні",
+              },
+              {
+                title: "Замовлення",
+                description: "Підсумок за замовленням",
+              },
+            ]}
+          />
+          <Divider style={{ marginTop: "0.55rem" }} />
+        </>
+      );
+    }
+    return null;
   };
 
   return (
     <Modal
-      //   title="Basic Modal"
       open={isModalOpen}
       onOk={handleOk}
       onCancel={handleCancel}
+      okText={okText}
+      cancelText="Назад"
+      okButtonProps={{ size: "large", style: { background: "black" } }}
       cancelButtonProps={{
+        size: "large",
         style: {
-          display: "none",
+          display: currentStep === 3 ? "none" : "inline-block",
         },
       }}
     >
-      <Steps
-        current={0}
-        items={[
-          {
-            title: "Доставка",
-            description: "Оберіть спосіб доставки",
-          },
-          {
-            title: "Контакти",
-            description: "Заповніть контактні данні",
-          },
-          {
-            title: "Замовлення",
-            description: "Підсумок за замовленням",
-          },
-        ]}
-      />
-      <Divider style={{ marginTop: "0.55rem" }} />
-      <ShippingTabButtons
-        currentTab={currentTab}
-        handleTabChange={handleTabChange}
-      />
-
-      <Tabs
-        activeKey={currentTab}
-        items={items}
-        renderTabBar={() => <></>}
-        animated
-      />
-      <LocationInfo />
+      {renderStepsAndDivider()}
+      {renderContent()}
     </Modal>
   );
 };
